a135b7e8d964d61a924504c7aaa893d91160b774
Mark Ducommun <ducommun.mark@gmail.com>
2017-05-07 17:34:31 -0500

    second test, it runs the setup method - failing

--- src/main/kotlin/io/ducommun/kUnit/WasRun.kt
+++ src/main/kotlin/io/ducommun/kUnit/WasRun.kt
@@ -5,6 +5,7 @@ class WasRun : TestCase(name = "testMethod") {
     private var wasRunPrivate: Boolean = false
 
     val wasRun: Boolean get() = wasRunPrivate
+    val wasSetup: Boolean get() = false
 
     fun testMethod(): Unit {
         wasRunPrivate = true
--- src/test/kotlin/io/ducommun/kUnit/mainTest.kt
+++ src/test/kotlin/io/ducommun/kUnit/mainTest.kt
@@ -4,10 +4,22 @@ import org.assertj.core.api.KotlinAssertions.assertThat
 
 
 fun main(args: Array<String>) {
-    TestCaseTest().run()
+    TestCaseTest("it tracks whether the setup method was run").run()
+    TestCaseTest("it tracks whether the test method was run").run()
 }
 
-class TestCaseTest : TestCase(name = "it tracks whether the test method was run") {
+class TestCaseTest(name: String) : TestCase(name = name) {
+
+    fun `it tracks whether the setup method was run`() {
+
+        val test = WasRun()
+
+        assertThat(test.wasSetup).isFalse()
+
+        test.run()
+
+        assertThat(test.wasSetup).isTrue()
+    }
 
     fun `it tracks whether the test method was run`() {
 
@@ -20,3 +32,5 @@ class TestCaseTest : TestCase(name = "it tracks whether the test method was run"
         assertThat(test.wasRun).isTrue()
     }
 }
+
+class Test
